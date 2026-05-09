6a5082ffbaab154240e2550417c1d30b56bae519
hyerim-moon <mhl0921@gmail.com>
2026-05-09 23:30:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -315,8 +315,8 @@
       </div>
       <div class="parent-row">
         ${parentLine(b.father, b.mother, b.fatherDeceased, b.motherDeceased)}
-        <span class="parent-dot" style="margin-left:3px;">●</span>
-         &nbsp;&nbsp;의 딸 <span class="child-name">${b.name}</span>
+        <span class="parent-dot">●</span>
+        <span style="padding-left:6px;">&nbsp;의 딸 <span class="child-name">${b.name}</span>
       </div>
     `;
 
